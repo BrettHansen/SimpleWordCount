4f2a70007fa2898d7be66f6a10b43f21d258ab5e
Brett Hansen <hansen.brett7@gmail.com>
2017-03-23 12:13:05 -0700

    Changed name; added continous updating feature.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WordCount Sublime Plugin
+# SimpleWordCount Sublime Plugin
 
 This is a very simple ST3 plugin to count words, characters, and lines in the current file.
 
@@ -11,3 +11,12 @@ Simply put the entire `WordCount` directory into your ST3 `Packages` directory.
 You can choose which counts to show by modifying the settings file.
 
 To show a popup with your selected counts, select "Word Count" from the context menu (right click in any file to show the context menu). Alternatively, press `ctrl+shift+c` on Windows or `command+shift+c` on Mac to show your selected counts in the status bar.
+
+## Settings
+
+The following settings are available for customization:
+* `show_word_count`: show count of words,
+* `show_char_count`: show count of characters,
+* `show_line_count`: show count of lines,
+* `run_continuously`: update status bar whenever the file is modified, and,
+* `time_delay`: time in seconds to wait between updates when running continuously.
--- SimpleWordCount.py
+++ SimpleWordCount.py
@@ -1,4 +1,7 @@
-import sublime, sublime_plugin, re
+import sublime, sublime_plugin, re, time
+
+settings = {}
+lastTime = time.time()
 
 class WordCountCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -11,7 +14,7 @@ class WordCountCommand(sublime_plugin.TextCommand):
 
 class WordCountPopupCommand(sublime_plugin.TextCommand):
 	def run(self, edit, event):
-		pos = self.view.layout_to_text((0, event["y"]))
+		pos = self.view.window_to_text((event["x"], event["y"]))
 		status = Helper.getAsHTML(self.view)
 
 		if status:
@@ -20,11 +23,24 @@ class WordCountPopupCommand(sublime_plugin.TextCommand):
 	def want_event(self):
 		return True
 
+class WordCountListener(sublime_plugin.EventListener):
+	def on_modified_async(self, view):
+		global lastTime
+		
+		prefs = Helper.getSettings()
+		if prefs["watch"] and time.time() - lastTime > prefs["delay"]:
+			lastTime = time.time()
+			status = Helper.getAsString(view)
+
+			if status:
+				view.set_status("word_count", status)
+			else:
+				view.erase_status("word_count")
 
 class Helper():
 	def getAsString(view):
 		prefs = Helper.getSettings()
-		words, chars, lines = Helper.countWords(view)
+		words, chars, lines = Helper.countWords(view, prefs)
 
 		return Helper.toString(Helper.toArray(words if prefs["words"] else None,
 											  chars if prefs["chars"] else None,
@@ -32,28 +48,34 @@ class Helper():
 
 	def getAsHTML(view):
 		prefs = Helper.getSettings()
-		words, chars, lines = Helper.countWords(view)
+		words, chars, lines = Helper.countWords(view, prefs)
 
 		return Helper.toHTML(Helper.toArray(words if prefs["words"] else None,
 											chars if prefs["chars"] else None,
 											lines if prefs["lines"] else None))
 
 	def getSettings():
+		global settings
+
+		if settings:
+			return settings
 		settings = {}
-		set_file = sublime.load_settings("WordCount.sublime-settings")
+		set_file = sublime.load_settings("SimpleWordCount.sublime-settings")
 		settings["words"] = set_file.get("show_word_count", True)
 		settings["chars"] = set_file.get("show_char_count", True)
 		settings["lines"] = set_file.get("show_line_count", True)
+		settings["watch"] = set_file.get("run_continuously", False)
+		settings["delay"] = set_file.get("time_delay", 1)
 		return settings
 
-	def countWords(view):
+	def countWords(view, prefs):
 		region = sublime.Region(0, view.size())
 		content = view.substr(region).strip()
 		p = re.compile("\s+", re.MULTILINE)
 		
-		words = len(re.split(p, re.sub("[,.?!:;]", " ", content))) - 1
-		chars = len(re.sub(p, "", content))
-		lines = len(view.lines(region))
+		words = len(re.split(p, re.sub("[,.?!:;]", " ", content))) - 1 if prefs["words"] else None
+		chars = len(re.sub(p, "", content)) if prefs["chars"] else None
+		lines = len(view.lines(region)) if prefs["lines"] else None
 
 		return (words, chars, lines)
 
